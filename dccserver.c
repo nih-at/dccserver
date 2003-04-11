b476a688570326ea3e693c9feebf21ff2d3ca825
Thomas Klausner <tk@giga.or.at>
2003-04-11 09:55:16 +0000

    Listen on multiple ports specified by -p.

--- dccserver.c
+++ dccserver.c
@@ -1,4 +1,4 @@
-/* $NiH: dccserver.c,v 1.43 2003/04/07 12:00:34 wiz Exp $ */
+/* $NiH: dccserver.c,v 1.44 2003/04/11 08:31:08 wiz Exp $ */
 /*-
  * Copyright (c) 2002, 2003 Thomas Klausner.
  * All rights reserved.
@@ -93,6 +93,11 @@ typedef struct child_s {
 } child_t;
 
 child_t children[NO_OF_CHILDREN];
+
+/* XXX: could be dynamic but that's probably more trouble than it's worth */
+#define NO_OF_LISTEN_PORTS 100
+int listen_port[NO_OF_LISTEN_PORTS];
+int listen_socket[NO_OF_LISTEN_PORTS];
 
 void
 say(const char *line, FILE *fp)
@@ -538,78 +543,12 @@ handle_input(void)
     return 0;
 }
 
-/*
- * set up server listening on connections and input from user, and
- * call appropriate functions to handle them
- */
+/* create a socket and bind it to port "port" */
 int
-main(int argc, char *argv[])
+create_and_bind_socket(int port)
 {
-    char *endptr;
     int sock, sock_opt;
-    int c;
-    int pollret;
-    long port;
     struct sockaddr_in laddr;
-    struct pollfd pollset[2];
-
-    strlcpy(nickname, "dccserver", sizeof(nickname));
-    /* do not echo lines entered by default */
-    echo_input = 0;
-    /* default to filtering out control characters */
-    filter_control_chars = 1;
-    port = 59;
-    prg = argv[0];
-
-    while ((c=getopt(argc, argv, "ehin:p:v")) != -1) {
-	switch(c) {
-	case 'h':
-	    usage();
-
-	case 'e':
-	    echo_input = 1;
-	    break;
-
-	case 'i':
-	    filter_control_chars = 0;
-	    break;
-
-	case 'v':
-	    puts(PACKAGE_STRING);
-	    exit(0);
-
-	case 'n':
-	    strlcpy(nickname, optarg, sizeof(nickname));
-	    break;
-
-	case 'p':
-	    port = strtol(optarg, &endptr, 10);
-	    if (*optarg == '\0' || *endptr != '\0')
-		usage();
-	    if (port < 0 || port > 65535)
-		err(1, "invalid port argument (0 <= port < 65536)");
-
-	    break;
-
-	default:
-	    /* unreachable */
-	    exit(1);
-	}
-    }
-
-    /* trying to imprison dccserver in a chroot */
-    if (chroot(".") == -1) {
-	if (errno == EPERM) {
-	    fputs("dccserver is not setuid root.\n"
-		  "For binding below port 1024 (like the default port 59),\n"
-		  "root privileges are needed.  Additionally, dccserver\n"
-		  "will chroot(2) itself to the current directory to reduce\n"
-		  "effect of any exploits.\n", stderr);
-	}
-	warn("can't chroot");
-    }
-    else if (chdir("/") == -1)
-	warn("can't chdir to \"/\" in chroot");
 
     if ((sock=socket(AF_INET, SOCK_STREAM, 0)) == -1)
 	err(1, "can't open socket");
@@ -637,15 +576,155 @@ main(int argc, char *argv[])
 	err(1, "can't bind socket to port %ld", port);
     }
 
+    return sock;
+}
+
+/* open sockets for all ports on which we should listen. */
+void
+create_and_bind_all_sockets(void)
+{
+    int i;
+
+    for (i=0; i < NO_OF_LISTEN_PORTS && listen_port[i] != -1; i++)
+	listen_socket[i] = create_and_bind_socket(listen_port[i]);
+
+    return;
+}
+
+/*
+ * remember to listen on port "port" too; avoid listening on a port
+ * twice.
+ */
+void
+add_listen_port(int port)
+{
+    int i;
+
+    for (i=0; i<NO_OF_LISTEN_PORTS; i++) {
+	if (listen_port[i] == port) {
+	    warnx("already listening on port %d", port);
+	    return;
+	}
+
+	if (listen_port[i] == -1) {
+	    listen_port[i] = port;
+	    return;
+	}
+    }
+
+    errx(1, "too many listening ports specified, maximum of %d allowed",
+	 NO_OF_LISTEN_PORTS);
+}
+
+/* call listen() for each socket on which we should */
+void
+start_listening(void)
+{
+    int i;
+
+    for (i=0; i < NO_OF_LISTEN_PORTS && listen_port[i] != -1; i++)
+	if (listen(listen_socket[i], BACKLOG) == -1)
+	    err(1, "can't listen on socket");
+
+    return;
+}
+/*
+ * set up server listening on connections and input from user, and
+ * call appropriate functions to handle them
+ */
+int
+main(int argc, char *argv[])
+{
+    char *endptr;
+    int c, i;
+    int pollret;
+    int port, port_count;
+    struct pollfd *pollset;
+
+    strlcpy(nickname, "dccserver", sizeof(nickname));
+    /* do not echo lines entered by default */
+    echo_input = 0;
+    /* default to filtering out control characters */
+    filter_control_chars = 1;
+    for (i=0; i<NO_OF_LISTEN_PORTS; i++) {
+	listen_port[i] = -1;
+	listen_socket[i] = -1;
+    }
+    port_count = 0;
+    prg = argv[0];
+
+    while ((c=getopt(argc, argv, "ehin:p:v")) != -1) {
+	switch(c) {
+	case 'h':
+	    usage();
+
+	case 'e':
+	    echo_input = 1;
+	    break;
+
+	case 'i':
+	    filter_control_chars = 0;
+	    break;
+
+	case 'v':
+	    puts(PACKAGE_STRING);
+	    exit(0);
+
+	case 'n':
+	    strlcpy(nickname, optarg, sizeof(nickname));
+	    break;
+
+	case 'p':
+	    port = strtol(optarg, &endptr, 10);
+	    if (*optarg == '\0' || *endptr != '\0')
+		usage();
+	    if (port < 1 || port > 65535)
+		errx(1, "invalid port argument (0 < port < 65536)");
+	    add_listen_port(port);
+	    port_count++;
+
+	    break;
+
+	default:
+	    /* unreachable */
+	    exit(1);
+	}
+    }
+
+    /* listen on port 59 by default */
+    if (listen_port[0] == -1) {
+	listen_port[0] = 59;
+	port_count = 1;
+    }
+
+    /* trying to imprison dccserver in a chroot */
+    if (chroot(".") == -1) {
+	if (errno == EPERM) {
+	    fputs("dccserver is not setuid root.\n"
+		  "For binding below port 1024 (like the default port 59),\n"
+		  "root privileges are needed.  Additionally, dccserver\n"
+		  "will chroot(2) itself to the current directory to reduce\n"
+		  "effect of any exploits.\n", stderr);
+	}
+	warn("can't chroot");
+    }
+    else if (chdir("/") == -1)
+	warn("can't chdir to \"/\" in chroot");
+
+    create_and_bind_all_sockets();
+
     /* drop privileges */
     setuid(getuid());
 
-    if (listen(sock, BACKLOG) == -1)
-	err(1, "can't listen on socket");
+    start_listening();
 
     for (c=0; c<NO_OF_CHILDREN; c++)
 	children[c].pid = -1;
     signal(SIGCHLD, sig_handle);
+
+    /* one for stdin, one for each listening socket */
+    if ((pollset=malloc(sizeof(*pollset)*(port_count+1))) == NULL)
+	err(1, "can't malloc pollset");
 
     while (1) {
 	struct sockaddr_in raddr;
@@ -675,14 +754,18 @@ main(int argc, char *argv[])
 	}
 
 	/* poll for events from network and user */
-	pollset[0].fd = 0;
-	pollset[0].events = POLLIN|POLLPRI;
-	pollset[0].revents = 0;
-	pollset[1].fd = sock;
-	pollset[1].events = POLLIN|POLLPRI;
-	pollset[1].revents = 0;
+	/* user */
+	pollset[port_count].fd = 0;
+	pollset[port_count].events = POLLIN|POLLPRI;
+	pollset[port_count].revents = 0;
+	/* network */
+	for (i=0; i<port_count; i++) {
+	    pollset[i].fd = listen_socket[i];
+	    pollset[i].events = POLLIN|POLLPRI;
+	    pollset[i].revents = 0;
+	}
 
-	if ((pollret=poll(pollset, 2, 500)) == -1) {
+	if ((pollret=poll(pollset, port_count+1, 500)) == -1) {
 	    if (errno != EINTR)
 		warn("poll error");
 	    continue;
@@ -691,28 +774,30 @@ main(int argc, char *argv[])
 	if (pollret == 0)
 	    continue;
 
-	if (pollset[0].revents != 0) {
+	if (pollset[port_count].revents != 0) {
 	    /* some data from stdin */
 
 	    if (handle_input() < 0)
 		break;
 	}
-	if (pollset[1].revents != 0) {
-	    /* some data from network */
-	    raddrlen = sizeof(raddr);
-	    if ((new_sock=accept(sock, (struct sockaddr *)&raddr,
-				 &raddrlen))  == -1) {
-		if ((errno != EINTR) && (errno != ECONNABORTED))
-		    warn("accept failed");
-		continue;
+	for (i=0; i<port_count; i++) {
+	    if (pollset[i].revents != 0) {
+		/* some data from network */
+		raddrlen = sizeof(raddr);
+		if ((new_sock=accept(listen_socket[i], (struct sockaddr *)&raddr,
+				     &raddrlen)) == -1) {
+		    if ((errno != EINTR) && (errno != ECONNABORTED))
+			warn("accept failed");
+		    continue;
+		}
+
+		(void)printf("Connection from %s/%d\n",
+			     inet_ntoa(raddr.sin_addr), ntohs(raddr.sin_port));
+		fflush(stdout);
+
+		/* do something */
+		handle_connection(new_sock, listen_socket[i]);
 	    }
-
-	    (void)printf("Connection from %s/%d\n",
-			 inet_ntoa(raddr.sin_addr), ntohs(raddr.sin_port));
-	    fflush(stdout);
-
-	    /* do something */
-	    handle_connection(new_sock, sock);
 	}
     }
 
